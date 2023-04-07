1b781396216f3fd14355f13339fb1572311dbfc6
Nazar Ilchenko <nilchenko2020@gmail.com>
2023-04-07 13:59:16 +0300

    Final version

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+
+const Swiper = require(swiper);
+
+const swiper = new Swiper(".swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
